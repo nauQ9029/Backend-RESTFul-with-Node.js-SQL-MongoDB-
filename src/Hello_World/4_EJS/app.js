21086b08fb43de664bc113cc3d713516989d87b2
nauQ <155826493+nauQ9029@users.noreply.github.com>
2025-05-18 23:25:37 +0700

    Added express static file

--- src/Hello_World/4_EJS/app.js
+++ src/Hello_World/4_EJS/app.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-
+const express = require('express');
+const app = express();
+require('dotenv').config();
 // Check if the environment variables are loaded
 // console.log('>>> check env: ', process.env)
-
 const port = process.env.PORT|| 3000;
 const hostname = process.env.HOST_NAME;
+const path = require('path');
 
-// Congif template engine
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
+// Config template engine
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// Config static files
+app.use(express.static(path.join(__dirname, '../../public')));  // Goes up two levels from 4_EJS -> Hello_World -> src, and into public
 
 // Default route
 app.get('/', (req, res) => {
-  res.send('Goodbye, World!')
+  res.send('Goodbye, World & nodemon!');
 })
 
 // Custom route
 app.get('/wtf', (req, res) => {
-  res.send('<h1>Goodbye, World! Message sent from ground zero of the third impact.</h1>')
+  res.send('<h1>Goodbye, World! Message sent from ground zero of the third impact.</h1>');
 })
 
 // Custom route w/ EJS
 app.get('/wtf2', (req, res) => {
-  res.render('sample.ejs')
+  res.render('sample.ejs');
 })
 
 app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
